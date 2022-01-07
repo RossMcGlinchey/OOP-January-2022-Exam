33d01e4849373bca0578db1451f622c5a89bd7f2
Ross McGlinchey <51384383+RossMcGlinchey@users.noreply.github.com>
2022-01-07 16:31:44 +0000

    Solution Created and Github linked

---
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Github Link: https://github.com/RossMcGlinchey/OOP-January-2022-Exam
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
